82e014462fe703815cdba1329f5504132e25cade
GeraldMoon85 <129676869+GeraldMoon85@users.noreply.github.com>
2023-04-02 17:52:37 +0000

    gtag added

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,20 @@ const IndexPage = () => {
 
   return (
     <Layout disablePaddingBottom>
+
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P35QLLP"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
       <Helmet>
+        <script>
+          {`
+          ( function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P35QLLP');
+          `}
+        </script>
         {/* Google Analytics tracking code */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-YGZ2TZS8LZ"></script>
         <script>
